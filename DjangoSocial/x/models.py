e08c5696c6e8dff61471a9d5a8b0b26de9053e05
pollirrata <jorgegaona@yahoo.com>
2013-03-02 00:07:33 -0500

    adding twitter module

---
+++ DjangoSocial/x/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class TwitterQuery(models.Model) :
+    query = models.CharField(max_length = 100)
+    max_keywords = models.IntegerField
+
+    def __unicode__(self):
+        return self.query
+
+
